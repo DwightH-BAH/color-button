6aefed135288605dc244d09c37504c9fdd858dd0
Dwight Hill <hill_dwight@bah.com>
2022-07-04 14:54:20 -0400

    test button color

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Learn React');
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App/>)
+  // find an element with a role of button and ext of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name:'Change to blue'})
+
+  //expect the background color to change
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
+
+  //onClick
+  fireEvent.click(colorButton);
+
+  //expect a background color of blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
+
+  //expect the button text to be change to red
+  expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('intitial conditions',()=> {
+  render(<App/>);
+
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+  // check that the checkbox start out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+})
